577d9932141c2b668dc4339b34622bb12c60c7f0
Willie <carranzawillie@gmail.com>
2022-09-25 15:59:52 -0500

    Cleaned up comments from populatedb.js

--- populatedb.js
+++ populatedb.js
@@ -1,15 +1,5 @@
-#! /usr/bin/env node
-
-console.log('This script populates some test items, brands, categories and item instances to the database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/inventory?retryWrites=true');
-
-// Get arguments passed on command line
 var userArgs = process.argv.slice(2);
-/*
-if (!userArgs[0].startsWith('mongodb')) {
-    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-    return
-}
-*/
+
 var async = require('async');
 var User = require('./models/user');
 var Message = require('./models/message');
@@ -76,7 +66,6 @@ function createUsers(cb) {
             userCreate('Rafael2112', 'Stacy2112', 'regular', callback);
         }
         ],
-        // optional callback
         cb);
 }
 
@@ -99,7 +88,6 @@ async.series([
     createUsers,
     createMessages
 ],
-// Optional callback
 function(err, results) {
     if (err) {
         console.log('FINAL ERR: '+err);
@@ -108,7 +96,6 @@ function(err, results) {
         console.log('MESSAGES: '+messages);
         
     }
-    // All done, disconnect from database
     mongoose.connection.close();
 });
 
